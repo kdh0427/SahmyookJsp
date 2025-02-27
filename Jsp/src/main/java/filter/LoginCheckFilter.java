6a5d0e9f11249400057312d44992edf970941cc2
kdh0427 <166967751+kdh0427@users.noreply.github.com>
2025-02-27 17:45:01 +0900

    11일차

---
+++ Jsp/src/main/java/filter/LoginCheckFilter.java
@@ -0,0 +1,43 @@
+package filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+public class LoginCheckFilter implements Filter {
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession session = httpRequest.getSession(false);
+
+		boolean login = false;
+		if (session != null) {
+			if (session.getAttribute("MEMBER") != null) {
+				login = true;
+			}
+		}
+		if (login) {
+			chain.doFilter(request, response);
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/chap19/loginForm.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
